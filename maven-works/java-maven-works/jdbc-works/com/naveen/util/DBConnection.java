597c52871dca1730afa3da0275c7d014b678d77c
Naveen <adith.naveen@gmail.com>
2022-08-01 12:32:36 +0530

    updated props

--- maven-works/java-maven-works/jdbc-works/com/naveen/util/DBConnection.java
+++ maven-works/java-maven-works/jdbc-works/com/naveen/util/DBConnection.java
@@ -1,23 +1,35 @@
 package com.naveen.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
 	public static Connection getMysqlConnection() {
 		// load the driver 
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			return  DriverManager.getConnection
-						("jdbc:mysql://localhost/sapient_aug_2022", "root", "kanchan@1");
 			
+			Properties prop = new Properties(); 
+			prop.load(new FileInputStream("jdbc-works/db.properties"));
+			
+			Class.forName(prop.getProperty("driver"));
+			return  DriverManager.getConnection
+						(
+						prop.getProperty("url"), 
+						prop.getProperty("username", "root"), 
+						prop.getProperty("password"));
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} 
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
 		
 		return null; 
 	}
